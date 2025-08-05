34eba9f92cd4db7ab2bc0282285c910b723b8c37
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-06 01:28:39 +0800

    feat: add extra flake inputs for master, unstable, last

--- flake.nix
+++ flake.nix
@@ -3,38 +3,47 @@
     "https://cache.nixos.org/"
   ];
 
-  inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
-    systems.url = "github:nix-systems/default";
-    mnw.url = "github:Gerg-L/mnw";
-  };
-
   outputs =
     {
       nixpkgs-stable,
       systems,
       mnw,
       ...
-    }:
+    }@inputs:
     builtins.foldl' (a: b: a // b) { } (
       builtins.map (
         system:
         let
-          pkgs-stable = nixpkgs-stable.legacyPackages.${system};
+          pkgs = nixpkgs-stable.legacyPackages.${system};
+
+          extras = {
+            pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
+            pkgs-master = inputs.nixpkgs-master.legacyPackages.${system};
+            pkgs-last = inputs.nixpkgs-last.legacyPackages.${system};
+          };
         in
         rec {
           devShells.${system}.default = import ./nix/devShell.nix {
-            pkgs = pkgs-stable;
+            inherit pkgs;
             selfpkgs = packages.${system};
           };
           packages.${system} = rec {
             neovim = import ./nix/packages/neovim.nix {
-              pkgs = pkgs-stable;
-              inherit mnw;
+              inherit pkgs extras mnw;
             };
             default = neovim;
           };
         }
       ) (import systems)
     );
+
+  inputs = {
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-last.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
+    systems.url = "github:nix-systems/default";
+    mnw.url = "github:Gerg-L/mnw";
+  };
+
 }
