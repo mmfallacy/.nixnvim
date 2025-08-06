d5c5157b5a006faf24306f453a83bf0b0be10dad
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-07 02:38:57 +0800

    feat: migrate back to pkgs.lib.pipe instead of pipe-operators

--- nix/vimPlugins/default.nix
+++ nix/vimPlugins/default.nix
@@ -1,14 +1,18 @@
 { pkgs }:
-./.
-|> builtins.readDir
-|> pkgs.lib.mapAttrs' (
-  path: type:
-  if type != "regular" || path == "default.nix" then
-    pkgs.lib.nameValuePair path null
-  else
-    let
-      fname = path |> pkgs.lib.splitString "." |> builtins.head;
-    in
-    pkgs.lib.nameValuePair fname (pkgs.callPackage (./. + "/${path}") { })
-)
-|> pkgs.lib.filterAttrs (k: v: v != null)
+pkgs.lib.pipe ./. [
+
+  builtins.readDir
+
+  (pkgs.lib.mapAttrs' (
+    path: type:
+    if type != "regular" || path == "default.nix" then
+      pkgs.lib.nameValuePair path null
+    else
+      let
+        fname = path |> pkgs.lib.splitString "." |> builtins.head;
+      in
+      pkgs.lib.nameValuePair fname (pkgs.callPackage (./. + "/${path}") { })
+  ))
+
+  (pkgs.lib.filterAttrs (k: v: v != null))
+]
