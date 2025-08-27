086d019954ac14adb8228111b038b18ffaa74899
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-27 16:57:22 +0800

    feat: update live-preview.nvim

--- nix/vimPlugins/live-preview-nvim.nix
+++ nix/vimPlugins/live-preview-nvim.nix
@@ -6,12 +6,12 @@
 
 vimUtils.buildVimPlugin {
   pname = "live-preview.nvim";
-  version = "2025-01-24";
+  version = "2025-08-17";
   src = fetchFromGitHub {
     owner = "brianhuster";
     repo = "live-preview.nvim";
-    rev = "330a6ac95a4eece731117ca5956250821bae8010";
-    sha256 = "sha256-4RxXh3Qx03pW63SpDwhA0ALVkCWCRiR6OnAzurpp88o=";
+    rev = "5890c4f7cb81a432fd5f3b960167757f1b4d4702";
+    sha256 = "sha256-nWHytvuzWvFRilYCiMfzB64iZ+puXWrxJQfe1GtHJj8=";
   };
   # [brianhuster/live-preview.nvim#L24](https://github.com/brianhuster/live-preview.nvim/blob/6de1e71572673918e29cfc012c8dbfa59a2e464b/lua/livepreview/_spec.lua#L24) fails as it trips during neovimRequireCheckHook. Packages are not installed within the neovim context during build hence this fails.
   nvimSkipModules = [ "livepreview._spec" ];
