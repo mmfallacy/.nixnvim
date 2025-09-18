36c4478dee2d0777d61448b2313fee53dcca8cfa
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-19 02:48:05 +0800

    lsp: add proper goto definition, type def, references, implementations

--- nvimrc/lua/custom/plugins/lsp/keybinds.lua
+++ nvimrc/lua/custom/plugins/lsp/keybinds.lua
@@ -7,6 +7,7 @@ return function(_, bufnr)
   bufmap('<C-k>', vim.lsp.buf.signature_help)
   bufmap('<leader>ca', vim.lsp.buf.code_action)
   bufmap('<leader>rr', vim.lsp.buf.rename)
+  bufmap('gD', vim.lsp.buf.declaration)
 
   bufmap('<leader>dl', function()
     vim.diagnostic.open_float({ scope = 'line' })
@@ -18,4 +19,9 @@ return function(_, bufnr)
   bufmap('<leader>dd', function()
     builtins.diagnostics({ bufnr = 0 })
   end)
+
+  bufmap('gd', builtins.lsp_definitions)
+  bufmap('gt', builtins.lsp_type_definitions)
+  bufmap('gi', builtins.lsp_implementations)
+  bufmap('gr', builtins.lsp_references)
 end
