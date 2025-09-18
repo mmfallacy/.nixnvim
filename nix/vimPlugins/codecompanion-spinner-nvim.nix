52dab780d6a818621f251fbf915c5ec9e6fc1a3d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-19 00:50:47 +0800

    nix: update codecompanion-spinner-nvim.nix

--- nix/vimPlugins/codecompanion-spinner-nvim.nix
+++ nix/vimPlugins/codecompanion-spinner-nvim.nix
@@ -5,12 +5,12 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "codecompanion-spinner.nvim";
-  version = "2025-07-23";
+  version = "2025-09-11";
   src = fetchFromGitHub {
     owner = "franco-ruggeri";
     repo = "codecompanion-spinner.nvim";
-    rev = "1f53b8d402de2a5ee6f9d64793f5eda49ed12a64";
-    sha256 = "02yqbpg4550y6x3jvbqlx6qcw4sbrixd56mhws2fxrvpz2s09fr7";
+    rev = "c1fa2a84ea1aed687aaed60df65e347c280f4f22";
+    sha256 = "sha256-+lalwWE02YlLlU5zSqBotI5YstDuXtF8k0e6b7lxnhU=";
   };
   meta.homepage = "https://github.com/franco-ruggeri/codecompanion-spinner.nvim/";
   meta.hydraPlatforms = [ ];
