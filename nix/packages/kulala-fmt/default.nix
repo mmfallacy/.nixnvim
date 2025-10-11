dbf19af98e42d33ee4f20f6e20333cc9895f8406
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-12 00:25:25 +0800

    packages: kulala-fmt: base derivation that bootstraps dependencies and pkg source
    References:
    - https://github.com/NixOS/nixpkgs/blob/098292adb31909e9c0aa58c964ab4c7730851082/pkgs/by-name/cu/cup-docker/package.nix
    - https://github.com/happysalada/nixpkgs/tree/eb3b90ac41f00d310db14bfafaa4aa4492a988d8/pkgs/by-name/he/helix-gpt

---
+++ nix/packages/kulala-fmt/default.nix
@@ -0,0 +1,94 @@
+{
+  lib,
+  fetchFromGitHub,
+  bun,
+  stdenvNoCC,
+}:
+let
+  pin = lib.importTOML ./pin.toml;
+
+  src = fetchFromGitHub {
+    owner = "mistweaverco";
+    repo = "kulala-fmt";
+    rev = "v${pin.src.version}";
+    hash = pin.src.hash;
+  };
+
+  dependencies = stdenvNoCC.mkDerivation {
+    pname = "kulala-fmt-node_modules";
+    inherit src;
+    version = pin.src.version;
+
+    # Set up network access for bun install
+    impureEnvVars = lib.fetchers.proxyImpureEnvVars ++ [
+      "GIT_PROXY_COMMAND"
+      "SOCKS_SERVER"
+    ];
+
+    nativeBuildInputs = [
+      bun
+    ];
+    buildPhase = ''
+      runHook preBuild
+
+      export BUN_INSTALL_CACHE_DIR=$(mktemp -d)
+
+      bun install \
+        --no-progress \
+        --frozen-lockfile \
+        --force \
+        --ignore-scripts \
+
+        # --ignore-script to prevent node-gyp-build from running when installing @mistweaverco/tree-sitter-kulala
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/node_modules
+      cp -R ./node_modules $out
+
+      runHook postInstall
+    '';
+
+    outputHash = pin.dependencies.${stdenvNoCC.system}.hash;
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  };
+in
+# Use finalAttrs to respect overrides
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "kulala-fmt";
+  version = pin.src.version;
+  inherit src;
+
+  node_modules = dependencies;
+
+  configurePhase = ''
+    runHook preConfigure
+
+    ln -s ${finalAttrs.node_modules}/node_modules node_modules
+
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p $out
+    cp -r . $out
+
+    runHook postBuild
+  '';
+
+  meta = {
+    description = "Opinionated .http and .rest files linter and formatter";
+    homepage = "https://github.com/mistweaverco/kulala-fmt";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "kulala-fmt";
+    platforms = lib.platforms.all;
+  };
+})
