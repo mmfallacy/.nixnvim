f21110c780769d8120d4e3a039465b2bdca2853f
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-12-14 02:40:00 +0800

    opencode: use jsonc and feed all files in nix/packages/opencode/config

--- nix/packages/opencode/default.nix
+++ nix/packages/opencode/default.nix
@@ -8,13 +8,13 @@
 stdenvNoCC.mkDerivation {
   name = "opencode-wrapped";
   buildInputs = [ makeWrapper ];
-  src = ./.;
+  src = ./config;
 
   installPhase = ''
     mkdir -p $out/bin
     mkdir -p $out/share/opencode
 
-    cp -r $src/opencode.json $out/share/opencode
+    cp -r $src/. $out/share/opencode
 
     makeWrapper ${opencode}/bin/opencode $out/bin/opencode \
       --set XDG_CONFIG_HOME "$out/share" \
