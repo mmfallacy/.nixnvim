294a3445f0fe30a42d7ed07b08e57bc97427e085
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-12-14 23:19:07 +0800

    opencode: extract opencode folder onto root instead of nested in nix/packages

--- nix/packages/opencode.nix
+++ nix/packages/opencode.nix
@@ -4,20 +4,23 @@
   makeWrapper,
   lib,
   wrapperArgs ? [ ],
+  cfgLocation ? "${placeholder "out"}/share",
 }:
 stdenvNoCC.mkDerivation {
   name = "opencode-wrapped";
   buildInputs = [ makeWrapper ];
-  src = ./config;
+
+  src = ../../opencode;
 
   installPhase = ''
     mkdir -p $out/bin
-    mkdir -p $out/share/opencode
+    mkdir -p $out/share
 
-    cp -r $src/. $out/share/opencode
+    cp -r $src $out/share/
 
     makeWrapper ${opencode}/bin/opencode $out/bin/opencode \
-      --set XDG_CONFIG_HOME "$out/share" \
-      ${lib.escapeShellArgs wrapperArgs}
+      --set XDG_CONFIG_HOME "${cfgLocation}" \
+      ${lib.escapeShellArgs wrapperArgs} \
+      --run 'echo "$XDG_CACHE_HOME"'
   '';
 }
