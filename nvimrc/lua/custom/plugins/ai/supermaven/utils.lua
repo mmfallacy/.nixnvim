10923d4c5462d1700f9045cfc594386a42d9dce1
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-02-08 02:01:04 +0800

    fix: ai: supermaven statusline colors too low contrast

--- nvimrc/lua/custom/plugins/ai/supermaven/utils.lua
+++ nvimrc/lua/custom/plugins/ai/supermaven/utils.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.enable_statusline_hl()
   local hl_normal = vim.api.nvim_get_hl(0, { name = 'MiniStatuslineModeNormal' })
 
-  vim.api.nvim_set_hl(0, 'SupermavenOn', { fg = '#A3BE8C', bg = hl_normal.bg, bold = true }) -- green
-  vim.api.nvim_set_hl(0, 'SupermavenOff', { fg = '#BF616A', bg = hl_normal.bg, bold = true }) -- red
+  vim.api.nvim_set_hl(0, 'SupermavenOn', { fg = '#3A7D2B', bg = hl_normal.bg, bold = true }) -- green
+  vim.api.nvim_set_hl(0, 'SupermavenOff', { fg = '#B03A3A', bg = hl_normal.bg, bold = true }) -- red
 end
 
 function M.statusline_status()
