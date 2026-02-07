38c68d6819a0a40f8ed7dfe54ed0ee1cdc645bc7
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-02-08 02:25:10 +0800

    ai: supermaven: clarify toggle logic and disable cmp on supermaven enable

--- nvimrc/lua/custom/plugins/ai/supermaven/init.lua
+++ nvimrc/lua/custom/plugins/ai/supermaven/init.lua
@@ -13,12 +13,23 @@ function M.config(_, opts)
   -- Enable statusline highlights
   local utils = require('custom.plugins.ai.supermaven.utils')
   utils.enable_statusline_hl()
+  local has_cmp, cmp = pcall(require, 'cmp')
 
   vim.keymap.set({ 'n', 'v', 'i' }, '<S-Tab>', function()
-    _G.supermaven_enabled = not _G.supermaven_enabled
-    api.toggle()
-    -- Manually dispose inlay!
-    require('supermaven-nvim.completion_preview').on_dispose_inlay()
+    if _G.supermaven_enabled then
+      _G.supermaven_enabled = false
+      _G.cmp_enabled = true
+      api.stop()
+      -- Manually dispose inlay!
+      require('supermaven-nvim.completion_preview').on_dispose_inlay()
+    else
+      _G.supermaven_enabled = true
+      _G.cmp_enabled = false
+
+      -- Single line if statement 😆
+      _ = has_cmp and cmp.abort()
+      api.start()
+    end
     vim.cmd('redrawstatus')
   end)
 end
