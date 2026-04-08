fb5863f6d618e1cfcc82c97ffd8fff5dc13860d5
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-04-08 14:59:25 +0000

    nix: source difftastic-nvim from fork

--- nix/vimPlugins/difftastic-nvim.nix
+++ nix/vimPlugins/difftastic-nvim.nix
@@ -8,11 +8,19 @@ let
   difftastic = vimUtils.buildVimPlugin {
     pname = "difftastic.nvim";
     version = "0.0.9-unstable-2026-03-14";
+    # src = fetchFromGitHub {
+    #   owner = "clabby";
+    #   repo = "difftastic.nvim";
+    #   rev = "6041ef0244b3fecf3b7f07de9af8cfbf8dbc4945";
+    #   hash = "sha256-23NGKhytF3OsLJgdrC51IH/sIGoqe/yBfmPsZKHOMSk=";
+    # };
+
+    # Source first from own fork until #17 gets merged
     src = fetchFromGitHub {
-      owner = "clabby";
+      owner = "mmfallacy";
       repo = "difftastic.nvim";
-      rev = "6041ef0244b3fecf3b7f07de9af8cfbf8dbc4945";
-      hash = "sha256-23NGKhytF3OsLJgdrC51IH/sIGoqe/yBfmPsZKHOMSk=";
+      rev = "d53dbb1ea56907c391b89e1498d1930ba10e8eb3";
+      hash = "sha256-/UijzAriZhAoIHoRjRpffPbm+asfZXzo0oXfko1aZp8=";
     };
     meta.homepage = "https://github.com/clabby/difftastic.nvim/";
     meta.hydraPlatforms = [ ];
