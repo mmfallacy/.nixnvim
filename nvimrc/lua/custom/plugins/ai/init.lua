7ccfcdf7342186a6c04f2d36e7755b8cade5a809
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-08-24 17:13:46 +0000

    nvim: ai: disable 99

--- nvimrc/lua/custom/plugins/ai/init.lua
+++ nvimrc/lua/custom/plugins/ai/init.lua
@@ -3,6 +3,6 @@ return {
   -- require('custom.plugins.ai.codecompanion'),
   -- require('custom.plugins.ai.opencode'),
   -- require('custom.plugins.ai.minuet-ai'),
-  require('custom.plugins.ai.99'),
+  -- require('custom.plugins.ai.99'),
   require('custom.plugins.ai.supermaven'),
 }
